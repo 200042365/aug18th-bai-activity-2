a72228590d587d4a1f4b8eaf2a4a90befc1ce064
_nicorette_craire <200042365@student.kangan.edu.au>
2026-08-18 13:14:25 +1000

    step-7_FIN

--- activity-2/activity-2/script.js
+++ activity-2/activity-2/script.js
@@ -2,6 +2,7 @@ const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
 const count = document.getElementById("count");
+const clearBtn = document.getElementById("clearBtn");
 
     // Load saved tasks from Local Storage ( or start empty)
 const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
@@ -9,6 +10,10 @@ const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     // Save the tasks array to Local Storage
 function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function allTasksDone() {
+    localStorage.clear();
 }
 
     //draw every task on the screen from the tasks array
@@ -21,11 +26,25 @@ function render() {
         const li = document.createElement("li");    //makes new <li>
 
         const taskText = document.createElement("span");
-        taskText.textContent = task;    // puts task text inside
+        taskText.textContent = task.text;    // puts task text inside
+
+            // strike-through if task done
+        if (task.done) {
+            taskText.style.textDecoration = "line-through";
+            taskText.style.opacity = "0.5";
+        }
+
+        const doneBtn = document.createElement("span");
+        doneBtn.textContent = task.done ? "UNDO" : "MARK AS DONE";
+
+        doneBtn.addEventListener("click", () => {
+            tasks[i].done = !tasks[i].done;
+            saveTasks();
+            render();
+        });
 
         const deleteBtn = document.createElement("span");   //delete button
         deleteBtn.textContent = "DELETE";
-        // deleteBtn.title = "DELETE";
 
             // when the delete button is clicked: remove the task & redraw    
         deleteBtn.addEventListener("click", () => {
@@ -35,6 +54,7 @@ function render() {
         });
 
         li.appendChild(taskText);
+        li.appendChild(doneBtn);
         li.appendChild(deleteBtn);
         taskList.appendChild(li);
     }
@@ -45,11 +65,11 @@ function render() {
 
     // Adds a new task - to the array
 addBtn.addEventListener("click", () => {
-    const task = taskInput.value.trim(); 
+    const text = taskInput.value.trim(); 
 
-    if (task === "") return;    // do nothing if box is empty
+    if (text === "") return;    // do nothing if box is empty
 
-    tasks.push(task);        // add new task to end of array
+    tasks.push({text, done: false});        // add new task to end of array
     saveTasks();            // Save to local storage
     render();               // redraw the screen
     taskInput.value = "";   //clear input box
@@ -61,5 +81,11 @@ taskInput.addEventListener("keydown", (event) => {
     if (event.key === "Enter") addBtn.click();
 }); 
 
+
+clearBtn.addEventListener("click", () => {
+    tasks.length = 0;
+    localStorage.removeItem("tasks");
+    render();
+});
 
 render();   // shows list when page loads
